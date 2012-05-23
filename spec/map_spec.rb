9f329d9d02c861b3aed55928923902690a6dac8a
remi <remi@remitaylor.com>
2012-05-23 00:55:52 -0700

    Started adding a few specs for #map

--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -6,4 +6,48 @@ describe RackStack, "#map" do
   def app
     Rack::Lint.new @app
   end
+
+  it "first #map to match path 'wins'" do
+    @app = RackStack.new do
+      map("/"){ run simple_app(:first){ write "first" } }
+      map("/"){ run simple_app(:second){ write "second" } }
+    end
+
+    @app.trace.should == clean_trace(%{
+      map "/" do
+        run SimpleApp<first>
+      end
+      map "/" do
+        run SimpleApp<second>
+      end
+    })
+
+    get("/").body.should == "first"
+  end
+
+  it "using #use, #run, and #map" do
+    @app = RackStack.new do
+      use ResponseWrapperMiddleware
+      run simple_app(:default){ write "default" }
+
+      map "/foo" do
+        use ResponseWrapperMiddleware, "[foo]"
+        run simple_app(:foo){ write "foo" }
+
+        # TODO add inner-map
+      end
+    end
+
+    @app.trace.should == clean_trace(%{
+      use ResponseWrapperMiddleware
+      run SimpleApp<default>
+      map "/foo" do
+        use ResponseWrapperMiddleware, "[foo]"
+        run SimpleApp<foo>
+      end
+    })
+
+    get("/").body.should == "*default*"
+    get("/foo").body.should == "*[foo]foo[foo]*"
+  end
 end
